64d51805f609520248adcfbbb87f502514d07cda
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-03-06 00:43:36 -0500

    k

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,12 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { setIsLoading } from '../store/slices/isLoading.slice';
-// import destacado from "../assets/img/viniles.png"
 
 const Home = () => {
   const pathname = useLocation()
 
   useEffect(() => {
-      window.scrollTo(0, 0)
+    window.scrollTo(0, 0)
   }, [pathname]);
 
 
@@ -16,8 +15,41 @@ const Home = () => {
     <main id="home">
       <div className='producto_destacado'>
         <div className='img_destacado'></div>
-
+        <div className="fp_description">
+          <h4 className="txtkalan">Producto en D%scuento<br/><span>Vinil Glitter</span></h4>
+          {/* <p>Vinil Glitter importado. Medida standart (25 x 70cm). Con 10% de descuento hasta agotar stock</p> */}
+          <h5 className="txtkalan">$3.00</h5>
+          <div className="fp_btn">
+            <a href="#discover"><div className="discover">Discover</div></a>
+            <div className="add_cart" id="add_cart">AL CARRITO</div>
+          </div>
+        </div>
       </div>
+      
+      <div class="filter_contain" id="filter_contain">
+            <div class="all">
+                <h4 id="ancla_menu_productos">Mostrar Todos</h4>
+                <p>Todos los productos</p>
+            </div>
+            <div class="materiales">
+                <div class="viniles">
+                    <h4>Viniles</h4>
+                    <p>Disponibles <span id="count_viniles"></span></p>
+                </div>
+                <div class="cintas">
+                    <h4>Cintas</h4>
+                    <p>Disponibles <span id="count_cintas"></span></p>
+                </div>
+                <div class="apliques">
+                    <h4>Apliques</h4>
+                    <p>Disponibles <span id="count_apliques"></span></p>
+                </div>
+                <div class="decorables">
+                    <h4>Decorables</h4>
+                    <p>Disponibles <span id="count_decorables"></span></p>
+                </div>
+            </div>
+        </div>
     </main>
   );
 };
