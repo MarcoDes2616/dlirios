2cf772cac8222be3be5d63a66d77856e83898178
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-03-06 10:33:12 -0500

    parallax

--- src/components/Loadder.jsx
+++ src/components/Loadder.jsx
@@ -7,8 +7,6 @@ const Loadder = () => {
             <article id="loader" className="loader">
                 <img src={logo} alt="logo empresa" />
                 <h4 className="txtkalan">Bienvenidos...</h4>
-                {/* <p className="txtkalan">Nos sentimos felices de contar con tu confianza, la cual agradecemos felizmente y esperamos estar a la altura de tus exigencias y la exigencias de tus clientes...
-                </p> */}
                 <p>Estamos cargando productos increibles para ti...</p>
             </article>
         </div>
---
+++ src/components/Parallax.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Parallax = () => {
+    return (
+        <div className='parallax'>
+            <p className="txtkalan">En <span>D´Lirios Insumos</span> nos sentimos honrados de contar con tu confianza, la cual agradecemos felizmente y esperamos estar a la altura de tus exigencias y la exigencias de tus clientes...
+            </p>
+        </div>
+    );
+};
+
+export default Parallax;
