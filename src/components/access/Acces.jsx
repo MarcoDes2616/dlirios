f9d01de518b3318c0c65645a83f83f78c379670e
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-03-07 09:12:48 -0500

    f

--- src/components/access/Acces.jsx
+++ src/components/access/Acces.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./access.css"
 import ws from "../../assets/img/rrss/ws.png"
 import insta from "../../assets/img/rrss/insta.png"
 
 const Acces = () => {
     const token = window.localStorage.getItem("token")
+    const [rrss, setRrss] = useState(false)
+    const [login, setLogin] = useState(false)
 
-    console.log(token);
     return (
         <div className='access'>
-            <div className='user_wrapper'>
-                <i className={token ? 'bx bx-user-check bx-sm' : 'bx bx-user bx-sm'}></i>
+            <div className={ login ? 'user_wrapper hide' : 'user_wrapper'}>
+                <i onClick={() => setLogin(!login)} className={token ? 'bx bx-user-check bx-sm' : 'bx bx-user bx-sm'}></i>
                 <div className='users'>
                     <p>Iniciar Sesión</p>
                 </div>
             </div>
-            <div className='rrss_wrapper'>
-                <i className='bx bx-like bx-sm'></i>
+            <div className={ rrss ? 'rrss_wrapper hide' : 'rrss_wrapper'}>
+                <i onClick={() => setRrss(!rrss)} className='bx bx-like bx-sm'></i>
                 <div className='contact_us'>
                     <p>Conecta:</p>
                     <img src={insta} alt="logo instagram" />
