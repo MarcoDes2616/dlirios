a785a93407c40994a542e9502d3096c6f64dd65a
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-03-07 09:14:07 -0500

    Update Acces.jsx

--- src/components/access/Acces.jsx
+++ src/components/access/Acces.jsx
@@ -10,13 +10,13 @@ const Acces = () => {
 
     return (
         <div className='access'>
-            <div className={ login ? 'user_wrapper hide' : 'user_wrapper'}>
+            <div className={ !login ? 'user_wrapper hide' : 'user_wrapper'}>
                 <i onClick={() => setLogin(!login)} className={token ? 'bx bx-user-check bx-sm' : 'bx bx-user bx-sm'}></i>
                 <div className='users'>
                     <p>Iniciar Sesión</p>
                 </div>
             </div>
-            <div className={ rrss ? 'rrss_wrapper hide' : 'rrss_wrapper'}>
+            <div className={ !rrss ? 'rrss_wrapper hide' : 'rrss_wrapper'}>
                 <i onClick={() => setRrss(!rrss)} className='bx bx-like bx-sm'></i>
                 <div className='contact_us'>
                     <p>Conecta:</p>
