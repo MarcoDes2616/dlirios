ae28d292ffaa9b56b25bad0dcf90f98dda807645
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-05-19 10:47:01 -0500

    Update axios.js

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: https://dliriosbackv1-production.up.railway.app/api/v1
+    baseURL: "https://dliriosbackv1-production.up.railway.app/api/v1"
 })
