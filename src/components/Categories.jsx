5a410865d4d7ea068e323ef5efa0a62256eb5015
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-05-21 14:29:49 -0500

    index

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -7,46 +7,46 @@ import creaciones from "../assets/img/creaciones.jpg";
 import { NavLink } from "react-router-dom";
 
 const Categories = () => {
-    return (
-        <div className="categories">
-        <NavLink style={({isActive}) => {return {display: isActive && "none"}}} to={"/products/category/2"}>
-          <div className="category">
-            <img src={viniles} alt="" />
-            <h3>Viniles</h3>
-          </div>
-        </NavLink>
-        <NavLink style={({isActive}) => {return {display: isActive && "none"}}} to={"/products/category/3"}>
-          <div className="category">
-            <img src={cintas} alt="" />
-            <h3>Cintas</h3>
-          </div>
-        </NavLink>
-        <NavLink style={({isActive}) => {return {display: isActive && "none"}}} to={"/products/category/4"}>
-          <div className="category">
-            <img src={apliques} alt="" />
-            <h3>Apliques</h3>
-          </div>
-        </NavLink>
-        <NavLink style={({isActive}) => {return {display: isActive && "none"}}} to={"/products/category/5"}>
-          <div className="category">
-            <img src={decorables} alt="" />
-            <h3>Decorables</h3>
-          </div>
-        </NavLink>
-        <NavLink style={({isActive}) => {return {display: isActive && "none"}}} to={"/products/category/6"}>
-          <div className="category">
-            <img src={herramientas} alt="" />
-            <h3>Herramientas</h3>
-          </div> 
-        </NavLink>
-        <NavLink style={({isActive}) => {return {display: isActive && "none"}}} to={"/products/category/7"}>
-          <div className="category">
-            <img src={creaciones} alt="" />
-            <h3>Creaciones Dlirios</h3>
-          </div>
-        </NavLink>
-      </div>
-    );
+  return (
+    <div className="categories">
+      <NavLink style={({ isActive }) => { return { display: isActive && "none" } }} to={"/products/category/2"}>
+        <div className="category">
+          <img src={viniles} alt="" />
+          <h3>Viniles</h3>
+        </div>
+      </NavLink>
+      <NavLink style={({ isActive }) => { return { display: isActive && "none" } }} to={"/products/category/3"}>
+        <div className="category">
+          <img src={cintas} alt="" />
+          <h3>Cintas</h3>
+        </div>
+      </NavLink>
+      <NavLink style={({ isActive }) => { return { display: isActive && "none" } }} to={"/products/category/4"}>
+        <div className="category">
+          <img src={apliques} alt="" />
+          <h3>Apliques</h3>
+        </div>
+      </NavLink>
+      <NavLink style={({ isActive }) => { return { display: isActive && "none" } }} to={"/products/category/5"}>
+        <div className="category">
+          <img src={decorables} alt="" />
+          <h3>Decorables</h3>
+        </div>
+      </NavLink>
+      <NavLink style={({ isActive }) => { return { display: isActive && "none" } }} to={"/products/category/6"}>
+        <div className="category">
+          <img src={herramientas} alt="" />
+          <h3>Herramientas</h3>
+        </div>
+      </NavLink>
+      <NavLink style={({ isActive }) => { return { display: isActive && "none" } }} to={"/products/category/7"}>
+        <div className="category">
+          <img src={creaciones} alt="" />
+          <h3>Creaciones Dlirios</h3>
+        </div>
+      </NavLink>
+    </div>
+  );
 };
 
 export default Categories;
