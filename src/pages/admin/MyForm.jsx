9a137780d7908a22eaef54a348e84a27af318480
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-05-21 17:09:52 -0500

    update form

--- src/pages/admin/MyForm.jsx
+++ src/pages/admin/MyForm.jsx
@@ -57,7 +57,7 @@ const MyForm = () => {
           <div className="input_box">
             <label>Categoria:</label>
             <select required className="input_form" name="categoryId" id="">
-              <option disabled value="">
+              <option defaultChecked value="">
                 Elige una categoría
               </option>
               {categories?.map((category) => (
